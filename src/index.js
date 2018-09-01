6282a3f52b47d8b8065dacd4f15309ab56445e14
Bestun Gardi <bestun95@yahoo.com>
2018-09-01 15:02:15 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -10,39 +10,52 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  <div class="oldercoaster">
-  <p>Two grannies having the time of their life!</p>
-  <p>Passengers:</p>
-  <ul>
-    <li>Agnes</li>
-    <li>Muriel</li>
-  </ul>
-</div>
+  render() {
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends React.Component {
-  <div>
-  <p>You shouldn't look too far.</p>
-  <p>Sometimes, the solution is right in front of you.</p>
-</div>
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
-
 export class ButcherShop extends React.Component {
-  <div class="butcher-shop">
-  <p>Hello! We have the following products for sale today:</p>
-  <ul>
-    <li>Tenderloin</li>
-    <li>Short ribs</li>
-    <li>Beef shin</li>
-    <li>Ribeye</li>
-  </ul>
-</div>
+  render() {
+    const products = BUTCHER_PRODUCTS.map((p, idx) => (<li key={idx}>{p}</li>))
+    return (
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
 }
-
-
-ReactDOM.render(
-  <div>
-    render your components here! Refer back to the previous README if you are lost
-  </div>,
-  document.getElementById('root')
-)
+export class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
